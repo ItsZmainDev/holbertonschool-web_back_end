510ed911116d7baf10ac1f44885e4c300d7eeb60
root <zmainpro@gmail.com>
2025-07-29 10:28:22 +0200

    Task 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module for type annotated string concatenation function"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenate two strings."""
+    return str1 + str2
