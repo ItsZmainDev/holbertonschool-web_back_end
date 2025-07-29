eae3715090611c2728eac71d73d0d5e2f31e3614
root <zmainpro@gmail.com>
2025-07-29 10:48:43 +0200

    Task 3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module that defines a type-annotated function to_str"""
+
+
+def to_str(n: float) -> str:
+    """Retourne la représentation en chaîne de caractères d'un float"""
+    return str(n)
