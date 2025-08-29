130fcd0a51e33857c220c0b53afa27fe25bc3034
Martin.Clement <zmainpro@gmail.com>
2025-08-29 21:33:34 +0200

    Promise

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,14 @@
+// 3-all.js
+import { uploadPhoto, createUser } from './utils.js';
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then(([photo, user]) => {
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+      return true;
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+      return false;
+    });
+}
